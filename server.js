cef7b23b8f84e480856649c03909ec397b33a174
Alex Johnson <alex.johnson@evertrue.com>
2014-09-15 18:40:57 -0400

    Setup server.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log('Hello World')
+console.log("Server Starting...")
+
+// Module dependencies:
+var express = require("express")
+  , app = express()                               // Express
+  , http = require("http").createServer(app)      // HTTP
+  , bodyParser = require("body-parser");          // Body-parser
+
+/* Server config */
+
+// Set IP
+app.set("ip", "127.0.0.1");
+
+// Set Port
+app.set("port", 8080);
+
+// Support JSON requests
+app.use(bodyParser.json());
+
+// ROUTING
+// Handle route "GET /", as in "http://localhost:8080/"
+app.get("/", function(request, response) {
+
+  // Show a simple response message
+  response.send("Server is up and running");
+
+});
+
+// Start HTTP server
+http.listen(app.get("port"), app.get("ip"), function() {
+  console.log("Server up and running. Go to http://" + app.get("ip") + ":" + app.get("port"));
+});
